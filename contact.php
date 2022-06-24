bd1a0e91671682ce09bac068f7a99e5c821c8c15
Engelbertus Vione <engelbertvione@gmail.com>
2022-06-24 09:54:05 +0800

    final UI

--- contact.php
+++ contact.php
@@ -1,32 +1,18 @@
 <?php
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = 'From: My Contact Form';
+	$to = 'hello@lingko.id';
+	$subject = 'Wassup?';
 
-$mail_to = 'hello@lingko.id';
-$subject = 'Message from a site visitor '.$field_name;
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
-
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to hello@lingko.id');
-		window.location = 'contact_page.html';
-	</script>
-<?php
-}
+	if ($_POST['submit']) {
+		if (mail ($to, $subject, $body, $from)) {
+		echo '<p>Message Sent Successfully!</p>';
+		} else {
+		echo '<p>Ah! Try again, please?</p>';
+		}
+	}
 ?>
